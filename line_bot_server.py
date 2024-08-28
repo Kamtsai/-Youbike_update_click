6c48a0b8a192d145cd3200a3f8e0b945b75074c8
Kamtsai <kam3tsai@gmail.com>
2024-08-28 13:52:16 +0800

    Update line_bot_server.py

--- line_bot_server.py
+++ line_bot_server.py
@@ -3,8 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
-import threading
-from youbike_scraper import scrape_youbike, send_line_message
+from youbike_scraper import scrape_youbike
 
 app = Flask(__name__)
 
@@ -15,30 +14,21 @@ handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text.lower() == "更新youbike":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="正在更新 YouBike 数据，请稍候...")
-        )
-        threading.Thread(target=update_youbike).start()
-
-def update_youbike():
-    results = scrape_youbike()
-    if results:
-        message = "YouBike站點可借車輛數量:\n" + "\n".join(results)
-    else:
-        message = "未找到 YouBike 站点数据"
-    send_line_message(message)
+        results = scrape_youbike()
+        if results:
+            message = "YouBike站點可借車輛數量:\n" + "\n".join(results)
+        else:
+            message = "無法獲取YouBike數據，請稍後再試。"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 
 if __name__ == "__main__":
     app.run()
